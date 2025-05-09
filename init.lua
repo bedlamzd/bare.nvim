85e6c3712516b8533d2e965e749148709ca0b1a3
bedlamzd <vcs@bedlamzd.dev>
2025-05-09 23:29:18 +0200

    feat(conform): Use ruff to format python

--- init.lua
+++ init.lua
@@ -803,7 +803,7 @@ require('lazy').setup({
       formatters_by_ft = {
         lua = { 'stylua' },
         -- Conform can also run multiple formatters sequentially
-        -- python = { "isort", "black" },
+        python = { 'ruff_format' },
         sql = { 'sqlfmt' },
         markdown = { 'cbfmt', 'mdformat' },
         json = { 'jq' },
