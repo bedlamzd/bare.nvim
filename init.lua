43e2e8214e40b92d46c9a4a369cae2ac3b61f053
bedlamzd <vcs@bedlamzd.dev>
2025-05-09 23:29:04 +0200

    fix(lsp): Disable basedpyright import sorting
    Ruff already handles that

--- init.lua
+++ init.lua
@@ -719,7 +719,16 @@ require('lazy').setup({
       local servers = {
         -- clangd = {},
         -- gopls = {},
-        basedpyright = {},
+        basedpyright = {
+          settings = {
+            basedpyright = {
+              disableOrganizeImports = true,
+              analysis = {
+                diagnosticMode = 'workspace',
+              },
+            },
+          },
+        },
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
