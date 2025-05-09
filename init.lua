7a88c15f5eaf05bb4f1ff459f13c995807221e2b
bedlamzd <vcs@bedlamzd.dev>
2025-05-09 23:29:18 +0200

    feat(conform): Configure formats for various filetypes

--- init.lua
+++ init.lua
@@ -804,6 +804,10 @@ require('lazy').setup({
         lua = { 'stylua' },
         -- Conform can also run multiple formatters sequentially
         -- python = { "isort", "black" },
+        sql = { 'sqlfmt' },
+        markdown = { 'cbfmt', 'mdformat' },
+        json = { 'jq' },
+        jsonc = { 'jq' },
         --
         -- You can use 'stop_after_first' to run the first available formatter from the list
         -- javascript = { "prettierd", "prettier", stop_after_first = true },
