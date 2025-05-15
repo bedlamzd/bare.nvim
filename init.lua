a74defb6da67b4e811dc12b9ad6a6f97d6b9744d
bedlamzd <vcs@bedlamzd.dev>
2025-05-16 01:16:37 +0200

    feat(plugins): Enable guess-indent
    Evaluating the plugin, see if I run into any issues

--- init.lua
+++ init.lua
@@ -277,8 +277,7 @@ rtp:prepend(lazypath)
 require('lazy').setup({
   -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
 
-  -- TODO: Try it out
-  -- 'NMAC427/guess-indent.nvim', -- Detect tabstop and shiftwidth automatically
+  'NMAC427/guess-indent.nvim', -- Detect tabstop and shiftwidth automatically
 
   -- NOTE: Plugins can also be added by using a table,
   -- with the first argument being the link and the following
