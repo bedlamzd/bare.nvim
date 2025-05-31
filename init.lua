b5f00c32991bdb3b8d8a4a1320c7256a6f5a3afc
bedlamzd <vcs@bedlamzd.dev>
2025-05-31 04:57:50 +0200

    feat(yamlls): Use custom schemas for k8s CRDs
    Load custom schemas from `$XDG_DATA_HOME/k8s-schemas` and match them
    based on file name
    
    Quite likely this is brittle and also relies on file names, instead of
    contents like `apiVersion`. But good enough for now

--- init.lua
+++ init.lua
@@ -753,6 +753,29 @@ require('lazy').setup({
         lineFoldingOnly = true,
       }
 
+      ---@return fun(): [string, string]? schemas_iterator Over pairs of (schema_path, glob_pattern)
+      local k8s_schemas = function()
+        local schemas_dir = vim.fs.abspath(vim.fn.stdpath 'data' .. '/../k8s-schemas')
+        schemas_dir = vim.fs.normalize(schemas_dir)
+        vim.notify(schemas_dir)
+        if not vim.fn.isdirectory(schemas_dir) then
+          -- empty iterator
+          return function() end
+        end
+        local schemas = vim.fs.dir(schemas_dir, { depth = 1 })
+
+        return coroutine.wrap(function()
+          for name, type in schemas do
+            if type ~= 'file' or name == '_definitions.json' then
+              goto continue
+            end
+            local pattern = string.gsub(vim.fs.basename(name), '^(.*)%.json', '%1.yaml')
+            coroutine.yield { vim.fs.joinpath(schemas_dir, name), pattern }
+            ::continue::
+          end
+        end)
+      end
+
       -- Enable the following language servers
       --  Feel free to add/remove any LSPs that you want here. They will automatically be installed.
       --
@@ -805,6 +828,24 @@ require('lazy').setup({
           settings = {
             ['harper-ls'] = {
               linters = { SentenceCapitalization = false },
+            },
+          },
+        },
+        yamlls = {
+          settings = {
+            yaml = {
+              -- WARN: kubernetes support will be removed eventually https://github.com/redhat-developer/yaml-language-server/issues/307
+              -- NOTE: When names are not standard, a magic comment can be added
+              --  see https://github.com/redhat-developer/yaml-language-server?tab=readme-ov-file#using-inlined-schema
+              --  CRD lists
+              --    - https://github.com/fluxcd-community/flux2-schemas
+              --    - https://github.com/instrumenta/kubernetes-json-schema
+              --  How to get used schemas from cluster
+              --    - https://github.com/redhat-developer/yaml-language-server/issues/132#issuecomment-1403851309
+              schemas = vim.iter(k8s_schemas()):fold({}, function(acc, v)
+                acc[v[1]] = v[2]
+                return acc
+              end),
             },
           },
         },
