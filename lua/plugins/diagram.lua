a92caae7d4803b9c04e66a932412581a89f322f3
bedlamzd <vcs@bedlamzd.dev>
2025-09-06 15:05:47 +0200

    feat(diagram): Draw diagrams on manual request only
    Inside zellij images do not show, so auto-drawing only causes problems

--- lua/plugins/diagram.lua
+++ lua/plugins/diagram.lua
@@ -1,7 +1,26 @@
+---@module 'lazy'
+---@type LazyPluginSpec
 return {
   '3rd/diagram.nvim',
   dependencies = {
     '3rd/image.nvim',
   },
-  opts = {},
+  opts = {
+    events = {
+      -- render manually only, because of zellij
+      render_buffer = {},
+      clear_buffer = { 'BufLeave' },
+    },
+  },
+  keys = {
+    {
+      '<leader>pi',
+      function()
+        require('diagram').show_diagram_hover()
+      end,
+      mode = 'n',
+      ft = { 'markdown' },
+      desc = 'Show diagram in new tab',
+    },
+  },
 }
