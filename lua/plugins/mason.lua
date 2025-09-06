58bb5086a6a0d3a0edff41f4fcd6ecc477ecc2be
bedlamzd <vcs@bedlamzd.dev>
2025-09-06 20:20:27 +0200

    feat(mdformat): Add wikilinks support

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,5 +1,9 @@
+---@module 'lazy'
+---@type LazyPluginSpec
 return {
   'mason-org/mason.nvim',
+  ---@module 'mason'
+  ---@type MasonSettings
   opts = {
     registries = {
       'file:' .. vim.fn.stdpath 'config' .. '/mason-registry-override',
