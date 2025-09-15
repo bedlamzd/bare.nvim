5d283df39811ac6b0c76a34e538dadc6af63d690
bedlamzd <vcs@bedlamzd.dev>
2025-09-15 13:29:20 +0200

    refactor(lsp): Cleanup server settings

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -57,9 +57,8 @@ local get_lsp_capabilities = function()
   return capabilities
 end
 
+---@type table<string, table<> | fun(): table>
 local servers = {
-  -- clangd = {},
-  -- gopls = {},
   basedpyright = {
     settings = {
       basedpyright = {
@@ -70,32 +69,17 @@ local servers = {
       },
     },
   },
-  -- rust_analyzer = {},
-  -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
-  --
-  -- Some languages (like typescript) have entire language plugins that can be useful:
-  --    https://github.com/pmizio/typescript-tools.nvim
-  --
-  -- But for many setups, the LSP (`ts_ls`) will work just fine
-  -- ts_ls = {},
-  --
-
   lua_ls = {
-    -- cmd = { ... },
-    -- filetypes = { ... },
-    -- capabilities = {},
     settings = {
       Lua = {
         completion = {
           callSnippet = 'Replace',
         },
-        -- You can toggle below to ignore Lua_LS's noisy `missing-fields` warnings
-        -- diagnostics = { disable = { 'missing-fields' } },
       },
     },
   },
-  docker_compose_language_service = {},
-  dockerls = {},
+  docker_compose_language_service = nil,
+  dockerls = nil,
   harper_ls = {
     settings = {
       ['harper-ls'] = {
