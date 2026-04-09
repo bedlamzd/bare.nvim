0c370aea797035459e29cae484bfc093e3af0729
bedlamzd <maxim.borisov@bedlamzd.dev>
2026-04-09 22:42:24 +0200

    feat(treesitter): Language injection queries for python
    Allows adding `# language=lang` comments on blocks so that highlight and
    format works on them

---
+++ queries/python/injections.scm
@@ -0,0 +1,24 @@
+; extend
+; stolen from here https://github.com/zlkn/dotfiles/blob/0449b119f7b1ad145b94b0436b9bd7ee760f1fb4/vim/.config/nvim/queries/python/injections.scm
+
+((comment) @injection.language
+  (#gsub! @injection.language "#%s*language=%s*([%w%p]+)%s*" "%1")
+  .
+  (expression_statement
+    (assignment
+      right: (string
+        (string_start)
+        (string_content) @injection.content
+        (string_end)))))
+
+( (comment) @injection.language
+  (#gsub! @injection.language "#%s*language=%s*([%w%p]+)%s*" "%1")
+  .
+  (expression_statement
+    (assignment
+      left: (identifier)
+      right: (string
+        (string_start)
+        (string_content) @injection.content
+        (string_end)))) )
+
