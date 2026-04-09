f9f3223c5752f828b957129c70f9d18d5bbb3d7c
bedlamzd <maxim.borisov@bedlamzd.dev>
2026-04-09 21:54:10 +0200

    feat(conform): Use tombi to format toml files

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -37,6 +37,7 @@ return { -- Autoformat
       markdown = { 'injected', 'mdformat' },
       json = { 'jq' },
       jsonc = { 'jq' },
+      toml = { 'tombi' },
       --
       -- You can use 'stop_after_first' to run the first available formatter from the list
       javascript = { 'biome', 'prettierd', 'prettier', stop_after_first = true },
