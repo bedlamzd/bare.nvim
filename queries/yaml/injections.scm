152cb640b952e0b55c7f3480ec1c1761979a3213
bedlamzd <maxim.borisov@bedlamzd.dev>
2026-04-09 22:42:13 +0200

    feat(treesitter): Language injection queries for yaml
    Allows adding `# language=lang` comments on blocks so that highlight and
    format works on them

---
+++ queries/yaml/injections.scm
@@ -0,0 +1,68 @@
+; extend
+; stolen from here https://github.com/zlkn/dotfiles/blob/0449b119f7b1ad145b94b0436b9bd7ee760f1fb4/vim/.config/nvim/queries/yaml/injections.scm
+
+; Support Idea-style injection: # language=yaml
+; Injection for block_scalar queries
+((comment) @injection.language
+  (#gsub! @injection.language "#%s*language=%s*([%w%p]+)%s*" "%1")
+  .
+  (block_mapping_pair
+    value: (block_node
+      (block_scalar) @injection.content
+      ; Start parse only after block indicator
+      (#offset! @injection.content 0 1 0 0))))
+
+((comment) @injection.language
+  (#gsub! @injection.language "#%s*language=%s*([%w%p]+)%s*" "%1")
+  value: (block_node
+    (block_mapping
+      .
+      (block_mapping_pair
+        value: (block_node
+          (block_scalar) @injection.content
+          (#offset! @injection.content 0 1 0 0))))))
+
+; Injection for unquoted string scalsrs
+((comment) @injection.language
+  (#gsub! @injection.language "#%s*language=%s*([%w%p]+)%s*" "%1")
+  .
+  (block_mapping_pair
+    value: (flow_node
+      (plain_scalar
+        (string_scalar) @injection.content))))
+
+((comment) @injection.language
+  (#gsub! @injection.language "#%s*language=%s*([%w%p]+)%s*" "%1")
+  value: (block_node
+    (block_mapping
+      .
+      (block_mapping_pair
+        value: (flow_node
+          (plain_scalar
+            (string_scalar) @injection.content))))))
+
+; Injection for quoted string scalars
+((comment) @injection.language
+  (#gsub! @injection.language "#%s*language=%s*([%w%p]+)%s*" "%1")
+  .
+  (block_mapping_pair
+    value: (flow_node
+      [
+        (double_quote_scalar)
+        (single_quote_scalar)
+      ] @injection.content))
+  ; Don't parse last quote as bash sentence
+  (#offset! @injection.content 0 1 0 -1))
+
+((comment) @injection.language
+  (#gsub! @injection.language "#%s*language=%s*([%w%p]+)%s*" "%1")
+  value: (block_node
+    (block_mapping
+      .
+      (block_mapping_pair
+        value: (flow_node
+          [
+            (double_quote_scalar)
+            (single_quote_scalar)
+          ] @injection.content
+          (#offset! @injection.content 0 1 0 -1))))))
