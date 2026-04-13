02caace076d8b50815d9f157a4bb5c76010d15f0
bedlamzd <maxim.borisov@bedlamzd.dev>
2026-04-13 04:07:01 +0200

    refactor(lua_ls): Use `before_init` instead of `on_init`
    This is superficial, as I still don't understand how this config is
    used. On the surface, the config is used on client creation and
    `before_init` is called before running the server (which gets the config
    sent to it), so it seems like modifying config there is the right thing.
    
    But I'm not sure if that part is even used for server and how client
    itself uses the config. Does it see any further updates after
    instantiation? Does the config gets invalidated on edit?
    
    All I know that original `on_init` way is from `nvim-lspconfig` docs,
    which cannot be wrong and that indeed seemed to work.
    
    So the only real motivation here is to be consistent with other configs
    for `yamlls` and `jsonls`

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -48,13 +48,13 @@ local servers = {
     },
   },
   lua_ls = {
-    on_init = function(client)
-      if client.workspace_folders then
-        local path = client.workspace_folders[1].name
+    before_init = function(init_params, config)
+      if init_params.workspaceFolders then
+        local path = init_params.workspaceFolders[1].name
         if path ~= vim.fn.stdpath 'config' and (vim.uv.fs_stat(path .. '/.luarc.json') or vim.uv.fs_stat(path .. '/.luarc.jsonc')) then return end
       end
 
-      client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
+      config.settings.Lua = vim.tbl_deep_extend('force', config.settings.Lua, {
         runtime = {
           version = 'LuaJIT',
           path = { 'lua/?.lua', 'lua/?/init.lua' },
