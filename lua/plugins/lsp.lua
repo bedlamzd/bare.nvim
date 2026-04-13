4a854096269f693488704b08f6ff01cafc92328e
bedlamzd <maxim.borisov@bedlamzd.dev>
2026-04-13 04:03:37 +0200

    refactor(lsp): Adapt yamlls config for the new lsp API
    Gathering schemas is potentially costly, so I defer it until server is
    acually needed. Running in `before_init` just in case the server needs
    it (didn't check how it receives schemas)

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -82,38 +82,41 @@ local servers = {
       },
     },
   },
-  yamlls = function()
-    return {
-      settings = {
-        yaml = {
-          -- WARN: kubernetes support will be removed eventually https://github.com/redhat-developer/yaml-language-server/issues/307
-          -- NOTE: When names are not standard, a magic comment can be added
-          --  see https://github.com/redhat-developer/yaml-language-server?tab=readme-ov-file#using-inlined-schema
-          --  CRD lists
-          --    - https://www.schemastore.org/
-          --    - https://github.com/fluxcd-community/flux2-schemas
-          --    - https://github.com/instrumenta/kubernetes-json-schema
-          --    - https://github.com/datreeio/CRDs-catalog
-          --  How to get used schemas from cluster
-          --    - https://github.com/redhat-developer/yaml-language-server/issues/132#issuecomment-1403851309
-          --    - https://github.com/datreeio/CRDs-catalog?tab=readme-ov-file#crd-extractor
-          validate = true,
-          schemaStore = { enable = false, url = '' },
-          schemas = require('schemastore').yaml.schemas {
-            extra = vim.iter(k8s_schemas()):fold({}, function(acc, v)
-              schema_path, pattern = unpack(v)
-              acc[#acc + 1] = {
-                fileMatch = pattern,
-                name = vim.fs.basename(schema_path),
-                url = 'file://' .. schema_path,
-              }
-              return acc
-            end),
-          },
+  yamlls = {
+    -- NOTE: defer loading schemas until LS actually needed
+    before_init = function(init_params, config)
+      config.settings.yaml = vim.tbl_deep_extend('force', config.settings.yaml, {
+        schemas = require('schemastore').yaml.schemas {
+          extra = vim.iter(k8s_schemas()):fold({}, function(acc, v)
+            schema_path, pattern = unpack(v)
+            acc[#acc + 1] = {
+              fileMatch = pattern,
+              name = vim.fs.basename(schema_path),
+              url = 'file://' .. schema_path,
+            }
+            return acc
+          end),
         },
+      })
+    end,
+    settings = {
+      yaml = {
+        -- WARN: kubernetes support will be removed eventually https://github.com/redhat-developer/yaml-language-server/issues/307
+        -- NOTE: When names are not standard, a magic comment can be added
+        --  see https://github.com/redhat-developer/yaml-language-server?tab=readme-ov-file#using-inlined-schema
+        --  CRD lists
+        --    - https://www.schemastore.org/
+        --    - https://github.com/fluxcd-community/flux2-schemas
+        --    - https://github.com/instrumenta/kubernetes-json-schema
+        --    - https://github.com/datreeio/CRDs-catalog
+        --  How to get used schemas from cluster
+        --    - https://github.com/redhat-developer/yaml-language-server/issues/132#issuecomment-1403851309
+        --    - https://github.com/datreeio/CRDs-catalog?tab=readme-ov-file#crd-extractor
+        validate = true,
+        schemaStore = { enable = false, url = '' },
       },
-    }
-  end,
+    },
+  },
   jsonls = function()
     return {
       settings = {
