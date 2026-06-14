540c8af61b206ae5789805d8ef648bd51a62edce
bedlamzd <maxim.borisov@bedlamzd.dev>
2026-06-15 01:19:24 +0200

    refactor(plugins): Migrate treesitter bundle to vim.pack

--- lua/plugins/nvim-treesitter-context.lua
+++
@@ -1,7 +0,0 @@
-return {
-  'nvim-treesitter/nvim-treesitter-context',
-  opts = {
-    max_lines = vim.o.scrolloff, -- this space isn't used anyway, so let's make it useful
-    multiline_threshold = 2,
-  },
-}
